e44ac6f63986563ef56adce1c09d97d22a0bdb45
Jinho Choi <choijinho817@gmail.com>
2022-12-23 15:20:11 +0000

    change filename modle.rs to user.rs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,6 @@
 use mongodb::{Client, options::ClientOptions};
 use std::env;
-mod model;
+mod user;
 
 pub async fn connect()->Result<Client, mongodb::error::Error>{
     let mongo_url = env::var("MONGO_URL").expect("$MONGO_URL doesnt exist!");
