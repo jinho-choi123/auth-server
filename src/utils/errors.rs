06b69340751984d542d2296a6fbb68a070fd43c9
jinho-choi123 <choijinho817@gmail.com>
2023-01-01 12:45:57 +0000

    implement custom error type

---
+++ src/utils/errors.rs
@@ -0,0 +1,68 @@
+use serde::Serialize;
+use actix_web::{ResponseError, http::StatusCode, HttpResponse};
+use std::fmt;
+
+#[derive(Debug)]
+pub enum AppErrType {
+    DB_Err,
+    NotFound_Err,
+}
+
+#[derive(Debug)]
+pub struct AppErr {
+    pub message: Option<String>,
+    pub cause: Option<String>,
+    pub errorType: AppErrType,
+}
+impl fmt::Display for AppErr{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl AppErr {
+    pub fn new(message: Option<String>, cause: Option<String>, errorType: AppErrType) -> AppErr{
+        return AppErr { message: message, cause: cause, errorType: errorType }
+    }
+    pub fn message(&self) -> String {
+        match self{
+            AppErr{
+                message: Some(message), 
+                cause: _, 
+                errorType: _
+            } => message.clone(),
+            AppErr{
+                message: None, 
+                cause: _, 
+                errorType: AppErrType::DB_Err,
+            } => "Error occur while DB operations.".to_string(),
+            AppErr { 
+                message: None, 
+                cause: _, 
+                errorType: AppErrType::NotFound_Err 
+            } => "The request item was not found.".to_string(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct AppErrResponse {
+    pub error: String,
+}
+
+
+impl ResponseError for AppErr {
+    fn status_code(&self) -> StatusCode {
+        match self.errorType{
+            AppErrType::DB_Err => StatusCode::INTERNAL_SERVER_ERROR,
+            AppErrType::NotFound_Err => StatusCode::NOT_FOUND,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::build(self.status_code())
+            .json(AppErrResponse {
+                error: self.message(),
+            })
+    }
+}
