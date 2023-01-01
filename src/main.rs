9966b1bc8b2e1a21923cec96a6d9f042de3b15f0
jinho-choi123 <choijinho817@gmail.com>
2023-01-01 13:52:46 +0000

    initialize test db

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use actix_web::{App, HttpServer};
 mod routers;
 mod db;
 mod utils;
-use db::{connect, init_db};
+use db::{connect, init_db, init_test_db};
 use routers::check_server;
 use routers::users::create_user_api;
 
 #[actix_web::main]
 async fn main()->std::io::Result<()> {
     init_db(&connect().await.unwrap_or_else(|_|{panic!("Error occur while initializing DB")})).await;
+
+    //development purpose only!! 
+    init_test_db(&connect().await.unwrap_or_else(|_|{panic!("Error occur while initializing DB")})).await;
+
     HttpServer::new(|| {
         App::new()
             .service(check_server)
