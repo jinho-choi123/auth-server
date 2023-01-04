0052ca001e47190b13d0f9de4e5094d7add39654
jinho-choi123 <choijinho817@gmail.com>
2023-01-04 15:39:26 +0000

    add login user api

--- src/routers/users/create.rs
+++ src/routers/users/create.rs
@@ -1,11 +1,10 @@
 use actix_web::{post, delete, HttpResponse, Responder, web, error::ResponseError, http::StatusCode};
 use crate::db::user::User;
 use crate::db::model::{create_user};
-use mongodb::error::Error;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct createReq{
+struct createReq{
     email: String,
     password: String,
 }
---
+++ src/routers/users/login.rs
@@ -0,0 +1,17 @@
+use actix_web::{get,HttpResponse, Responder, web, error::ResponseError, http::StatusCode};
+use crate::db::model::{verify_user};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct loginReq{
+    email: String,
+    password: String,
+}
+
+#[get("/users/login")]
+pub async fn login_user_api(data: web::Json<loginReq>) -> impl Responder {
+    match verify_user(&data.email, &data.password).await {
+        Ok(jwt_token) => HttpResponse::Ok().body(jwt_token),
+        Err(v) => v.error_response(),
+    }
+}   
