4e4818d98ff6e6b4d3e86c30ad188cd39900817d
jinho-choi123 <choijinho817@gmail.com>
2023-02-07 03:45:57 +0000

    change implementation of refreshtokne endpoint

--- src/routers/users/refreshtoken.rs
+++ src/routers/users/refreshtoken.rs
@@ -9,16 +9,33 @@ struct refreshReq {
     refresh_token: String,
 }
 
+#[derive(Serialize)]
+struct refreshRes {
+    accessToken: String,
+    msg: String,
+}
+
 #[post("/jwt/refresh")]
-pub async fn refresh2access_api(data: web::Json<refreshReq>)-> impl Responder {
-    match validate_refresh_jwt(&data.refresh_token) {
-        Ok(()) => {
+pub async fn refresh2access_api(data: web::Json<refreshReq>)-> Result<web::Json<refreshRes>, AppErr>{
+    match validate_refresh_jwt(&data.refresh_token).await {
+        Ok(email) => {
             match refresh2access_jwt(&data.refresh_token) {
-                Ok(access_token) => HttpResponse::Ok().body(access_token),
-                Err(err) => err.error_response()
+                Ok(access_token) => 
+                    match store_refresh_jwt(&String::new(), &email).await {
+                        Ok(()) => Ok(
+                                    web::Json(
+                                        refreshRes {
+                                            accessToken: access_token,
+                                            msg: "refreshing token success.".to_string(),
+                                        }
+                                    )
+                                ),
+                        Err(err) => Err(err),
+                    },
+                Err(err) => Err(err),
             }
         
         },
-        Err(err) => err.error_response(),
+        Err(err) => Err(err),
     }
 }
