9e1048f589ce77ddd26bc3f62e306a33eec55a53
jinho-choi123 <choijinho817@gmail.com>
2023-02-07 02:16:41 +0000

    change delete user endpoint response

--- src/routers/users/delete.rs
+++ src/routers/users/delete.rs
@@ -1,12 +1,25 @@
 use actix_web::{post, delete, HttpResponse, Responder, web, error::ResponseError, http::StatusCode};
 use crate::db::user::User;
 use crate::db::model::{delete_dbuser};
+use crate::utils::errors::AppErr;
+use serde::{Serialize};
+
+#[derive(Serialize)]
+struct deleteRes{
+    msg: String,
+    email: String,
+}
 
 #[delete("/users/delete/{user_email}")]
-pub async fn delete_user_api(path: web::Path<String>) -> impl Responder{
+pub async fn delete_user_api(path: web::Path<String>) -> Result<web::Json<deleteRes>, AppErr> {
     let user_email = path.into_inner();
     match delete_dbuser(&user_email).await {
-    Ok(()) => HttpResponse::Ok().body(format!("User with email {} delete success!", user_email)),
-    Err(v) => v.error_response(),
+    Ok(()) => Ok(web::Json(
+        deleteRes {
+            msg: "User deletion success.".to_string(),
+            email: user_email,
+        }
+    )),
+    Err(v) => Err(v),
     }
 }
